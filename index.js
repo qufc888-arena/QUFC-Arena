347ed4b4e720bb100e7dc46fd50ebdab4e43d6c6
qufc888-arena <qufc888@gmail.com>
2026-03-17 10:59:15 +0700

    Require backend server in index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(__dirname));
-
-app.get("/", (req, res) => {
-  res.send("QUFC Arena Server Running");
-});
-
-app.listen(PORT, () => {
-  console.log("QUFC Arena running on port " + PORT);
-});
+require('./backend/server.js');
